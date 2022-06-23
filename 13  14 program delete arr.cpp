17a9edf93b23866fe315815f9c53eaaa077685de
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-06-23 12:16:17 +0500

    Add files via upload

---
+++ 13  14 program delete arr.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class array{
+	private:
+		int arr[10];
+		int n;
+		int pos;
+		public:
+			void in()
+			{
+				cout<<"Enter the Size of the array : ";
+				cin>>n;
+				cout<<"Enter the elements of the array : "<<endl;
+				for(int i=0 ;i< n; i++)
+				{
+					cin>>arr[i];
+				}
+			}
+			void delArr()
+				{
+					cout<<"Enter the postion to delete : "<<endl;
+					cin>>pos;
+					for(int i=pos; i<n; i++)
+					{
+						arr[i-1] = arr[i];
+					}
+					n = n - 1;
+					cout<<"The array after deletion is : ";
+					for(int i =0 ; i<n ; i++)
+					{
+						cout<<arr[i]<<endl;
+					}
+				}		
+};
+int main()
+{
+	array A ;
+	A.in();
+	A.delArr();
+}
