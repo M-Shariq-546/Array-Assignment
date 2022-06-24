9fa9c2760a3553b9b80fec2692341d7312a3d7f7
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-06-24 10:11:23 -0700

    Update 7 program lenght arr.cpp

--- 7 program lenght arr.cpp
+++ 7 program lenght arr.cpp
@@ -16,6 +16,12 @@ class array{
 			void arrlenght()
 			{
 				int sizearr = sizeof(arr)/sizeof(arr[0]);
+				/*sizeof(arr) = it indicate the whole size of the string 
+				such that integer takes 4 bytes so the total indices will be multiplied with 4 so 
+				the total size of array will be obtained .
+				And the sizeof(arr[0]) indicate the size of array with respect to only one index such that 4 is multiplied with 1
+				So the divisin of both sizes will gives us the lenght of the array
+				*/
 				cout<<"The Lenght of array is : "<<sizearr;
 			}
 };
