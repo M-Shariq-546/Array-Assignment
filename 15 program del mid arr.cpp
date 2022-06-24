b5ad3ef0998b461b40b70ba4cef557eb23825d49
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-06-24 10:25:13 -0700

    Update 15 program del mid arr.cpp

--- 15 program del mid arr.cpp
+++ 15 program del mid arr.cpp
@@ -13,12 +13,13 @@ class array{
 				cout<<"Enter the elements of the array : "<<endl;
 				for(int i=0 ;i< n; i++)
 				{
+					cout<<"No. "<<(i+1)<<" : ";
 					cin>>arr[i];
 				}
 			}
 			void delArr()
 				{
-					cout<<"Enter the postion to delete : "<<endl;
+					cout<<"Enter the postion to delete : ";
 					cin>>pos;
 					for(int i=pos; i<n; i++)
 					{
