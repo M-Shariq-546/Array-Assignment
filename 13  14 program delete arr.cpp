ef01ade99c70a1893b129080412e4ec9f7022284
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-07-06 00:33:20 +0500

    Logic Updated using Functions

--- 13  14 program delete arr.cpp
+++ 13  14 program delete arr.cpp
@@ -1,41 +1,36 @@
+//Program Taking value randomly and then Insert the value at start
 #include<iostream>
 using namespace std;
-class array{
-	private:
-		int arr[10];
-		int n;
-		int pos;
-		public:
-			void in()
-			{
-				cout<<"Enter the Size of the array : ";
-				cin>>n;
-				cout<<"Enter the elements of the array : "<<endl;
-				for(int i=0 ;i< n; i++)
-				{
-					cout<<"No . "<<(i+1)<<" : ";
-					cin>>arr[i];
-				}
-			}
-			void delArr()
-				{
-					cout<<"Enter the postion to delete : ";
-					cin>>pos;
-					for(int i=pos; i<n; i++)
-					{
-						arr[i-1] = arr[i];
-					}
-					n = n - 1;
-					cout<<"The array after deletion is : ";
-					for(int i =0 ; i<n ; i++)
-					{
-						cout<<arr[i]<<endl;
-					}
-				}		
-};
+void DeleteAtPos(int A[] , int s , int pos)
+{	
+	if(A[pos] != 0)
+	{
+		A[pos] = 0;
+	for(int i=0 ; i<s; i++)
+	{
+		cout<<A[i]<<endl;
+	}
+	cout<<"The value at index "<<pos<<" deleted successfully."<<endl;
+	}
+}
 int main()
 {
-	array A ;
-	A.in();
-	A.delArr();
+	int a[100];
+	const int size = 10;
+	for(int i=0 ;i<size ; i++)
+	{
+		a[i] = 0;
+	}
+	for(int i=0 ; i<size ; i++)
+	{
+		a[0]  = 5;
+		a[1] = 6;
+		a[2] = 546;
+		a[3] = 7;
+		a[4] = 8;
+		a[5] = 9;
+		a[6] = 10;
+	}
+	DeleteAtPos(a , size , 2);
+	return 0;
 }
