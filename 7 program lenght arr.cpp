cdb46e63dbb40fc4eadce05436b6bac029cd9468
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-07-06 00:40:49 +0500

    Logic updated using Functions

--- 7 program lenght arr.cpp
+++ 7 program lenght arr.cpp
@@ -1,34 +1,30 @@
+//Program Taking value randomly and then Insert the value at start
 #include<iostream>
 using namespace std;
-class array{
-	private:
-		int arr[10];
-		
-		public:
-			void in()
-			{
-				cout<<"Enter the elements of the array : "<<endl;
-				for(int i=0 ;i< 10; i++)
-				{
-					cin>>arr[i];
-				}
-			}
-			void arrlenght()
-			{
-				int sizearr = sizeof(arr)/sizeof(arr[0]);
-				/*sizeof(arr) = it indicate the whole size of the string 
-				such that integer takes 4 bytes so the total indices will be multiplied with 4 so 
-				the total size of array will be obtained .
-				And the sizeof(arr[0]) indicate the size of array with respect to only one index such that 4 is multiplied with 1
-				So the divisin of both sizes will gives us the lenght of the array
-				*/
-				cout<<"The Lenght of array is : "<<sizearr;
-			}
-};
+int count =0;
+void getLenght(int A[] , int s )
+{	
+	for(int i=0 ; i<s; i++)
+	{
+		if(A[i] != 0)
+		{
+			count++;
+		}
+	}
+	cout<<"The Lenght of the array is : "<<count<<endl;
+}
 int main()
 {
-	array A ;
-	A.in();
-	A.arrlenght();
-return 0;
+	int a[100];
+	const int size = 10;
+	for(int i=0 ;i<size ; i++)
+	{
+		a[i] = 0;
+	}
+	for(int i=0 ; i<size ; i++)
+	{
+		cin>>a[i];
+	}
+	getLenght(a , size);
+	return 0;
 }
