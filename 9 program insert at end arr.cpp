88dbaa3437f4b192efd320a928a892efd777b155
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-07-06 00:22:06 +0500

    Logicx Updated using functions

--- 9 program insert at end arr.cpp
+++ 9 program insert at end arr.cpp
@@ -1,34 +1,33 @@
+//Program Taking value randomly and then Insert the value at start
 #include<iostream>
 using namespace std;
-class array{
-	private:
-		int arr[6];
-		int num;
-		int i;
-		public:
-			void in()
-			{
-				cout<<"Enter the elements of the array : "<<endl;
-				for(i=0 ;i< 5; i++)
-				{
-					cin>>arr[i];
-				}
-			}
-			void insertArr()
-			{
-				cout<<"Enter the Number to insert in array : ";
-				cin>>num;
-				arr[i] = num;
-				for(i=0 ; i<6 ; i++)
-				{
-					cout<<arr[i]<<"\t";
-				}
-			}
-};
+void insertAtEnd(int A[] , int s ,int val)
+{	
+	if(A[s] == 0)
+	{
+		A[s-1] = val;
+		for(int i=0 ; i<s; i++)
+		{
+			cout<<A[i]<<endl;
+		}	
+	}
+}
 int main()
 {
-	array A ;
-	A.in();
-	A.insertArr();
+	int a[100];
+	const int size = 5;
+	for(int i=0 ;i<size ; i++)
+	{
+		a[i] = 0;
+	}
+	for(int i=0 ; i<size ; i++)
+	{
+		a[0] = 5;
+		a[2] = 7;
+		a[4] = 7;
+		a[5] = 9;
+		a[size - 1] = 8;
+	}
+	insertAtEnd(a , size , 546);
 	return 0;
 }
