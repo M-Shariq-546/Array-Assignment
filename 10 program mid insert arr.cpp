fa6b8c1c1f4b537e901b6af3a65e86f00e1858a3
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-07-06 00:29:32 +0500

    Logic updated using Functions

--- 10 program mid insert arr.cpp
+++ 10 program mid insert arr.cpp
@@ -1,55 +1,34 @@
+//Program Taking value randomly and then Insert the value at start
 #include<iostream>
 using namespace std;
-class array{
-	private:
-		int arr[10];
-		int size;
-		int position;
-		int num;
-		int i;
-		int mid;
-		public:
-			void in()
-			{
-				cout<<"Enter the size of the array : ";
-				cin>>size;
-				cout<<"Enter the elements of the array : "<<endl;
-				for(i=0 ;i<size; i++)
-				{
-					cout<<"No. "<<(i+1)<<" ";
-					cin>>arr[i];
-				}
-			}
-			void insertArr()
-			{
-				cout<<"Enter Position No. (1 - 9): ";
-				cin>>position;
-				if(position > 10)
-				{
-					cout<<"Out of range . "<<endl;
-					cout<<"Please insert from 1 - 9"<<endl;
-				}
-				else{
-				cout<<"Enter the Number to insert in array : ";
-				cin>>num;
-				--position; //To provide the position user wants 
-				for(int i = size ; i>= position ; i--)
-				{
-					arr[i+1] = arr[i];
-				}
-				arr[position] = num; // position will be the index and num will be placed at that place 
-				cout<<"The new array after insertion is : ";
-				for(int i=0 ; i<(size + 1); i++) // the size of array will be incremented by 1 automatically
-				{
-					cout<<arr[i]<<"\t";
-				}
-				}
-			}
-};
+void insertAtPos(int A[] , int s , int pos ,int val)
+{	
+	if(A[pos/2] == 0)
+	{
+		A[pos/2] = val;
+	for(int i=0 ; i<s; i++)
+	{
+		cout<<A[i]<<endl;
+	}
+	}
+}
 int main()
 {
-	array A ;
-	A.in();
-	A.insertArr();
+	int a[100];
+	const int size = 30;
+	for(int i=0 ;i<size ; i++)
+	{
+		a[i] = 0;
+	}
+	for(int i=0 ; i<size ; i++)
+	{
+		a[5]  = 5;
+		a[10] = 6;
+		a[16] = 7;
+		a[20] = 8;
+		a[25] = 9;
+		a[29] = 10;
+	}
+	insertAtPos(a , size , 7 , 546);
 	return 0;
 }
